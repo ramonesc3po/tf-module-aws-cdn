ed208b7335d0a6940824622751331e0a32574f39
Ramon <rsalado7729@gmail.com>
2019-01-18 15:00:46 -0200

    Add output iam_arn

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "path_cloudfront_origin_access_identity" {
   value = "${aws_cloudfront_origin_access_identity.default.cloudfront_access_identity_path}"
 }
+
+output "iam_arn_cloudfront_origin_access_identity" {
+  value = "${aws_cloudfront_origin_access_identity.default.iam_arn}"
+}
