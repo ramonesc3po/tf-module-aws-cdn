d7438257ff7f59a244ef823319300a9a5cb95a92
Ramon <rsalado7729@gmail.com>
2019-01-28 17:38:11 -0200

    Add output arn_cloudfront_this id_cloudfront_this

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "path_cloudfront_origin_access_identity" {
 output "iam_arn_cloudfront_origin_access_identity" {
   value = "${aws_cloudfront_origin_access_identity.default.iam_arn}"
 }
+
+output "arn_cloudfront_this" {
+  value = "${aws_cloudfront_distribution.this.arn}"
+}
+
+output "id_cloudfront_this" {
+  value = "${aws_cloudfront_distribution.this.id}"
+}
